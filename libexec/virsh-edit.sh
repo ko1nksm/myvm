434c60830c2f2708fad1bd2f6b5b5e5bae7d4c8b
Koichi Nakashima <koichi@nksm.name>
2023-07-12 00:23:46 +0900

    Refactor virsh-edit.sh

--- libexec/virsh-edit.sh
+++ libexec/virsh-edit.sh
@@ -12,25 +12,21 @@ append_vnc() {
     <graphics type="vnc" port="-1" autoport="yes" listen="0.0.0.0">\
       <listen type="address" address="0.0.0.0" />\
     </graphics>
-'
+  '
 }
 
 remove_vnc() {
   sed '/<graphics /,/<\/graphics>/d'
 }
 
-enable_vnc() {
-  overwrite "$1" remove_vnc
-  overwrite "$1" append_vnc
-}
-
-disable_vnc() {
-  overwrite "$1" remove_vnc
-}
-
 cmd=${MYVM_VIRSH_EDIT_COMMAND:-}
 case $cmd in
-  enable-vnc) enable_vnc "$@" ;;
-  disable-vnc) disable_vnc "$@" ;;
+  enable-vnc)
+    overwrite "$1" remove_vnc
+    overwrite "$1" append_vnc
+    ;;
+  disable-vnc)
+    overwrite "$1" remove_vnc
+    ;;
   *) echo "virsh-edit: Unknown command '$cmd'"
 esac
